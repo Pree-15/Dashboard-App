e8cd5a6306507fef6549c27fbdf1738b241817ad
Pree-15 <146547284+Pree-15@users.noreply.github.com>
2025-06-23 16:19:31 +0530

    Update Navbar.jsx

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,69 +1,107 @@
-import React, { useState } from 'react'
-import { 
-    LightModeOutlined, 
-    DarkModeOutlined, 
-    Menu as MenuIcon, 
-    Search, SettingsOutlined, 
-    ArrowDropDownOutlined,
+import React, { useState } from 'react';
+import {
+  LightModeOutlined,
+  DarkModeOutlined,
+  Menu as MenuIcon,
+  Search,
+  SettingsOutlined,
+  ArrowDropDownOutlined,
 } from '@mui/icons-material';
-import FlexBetween from 'components/FlexBetween';
+import {
+  AppBar,
+  IconButton,
+  InputBase,
+  Toolbar,
+  useTheme,
+  Typography,
+  Button,
+  Box,
+  Menu,
+  MenuItem,
+  Avatar
+} from '@mui/material';
 import { useDispatch } from 'react-redux';
 import { setMode } from 'state';
-import profileImage from 'assets/profile.jpg'
-import { AppBar, IconButton, InputBase, Toolbar, useTheme, Typography, Button,
-  Box, Menu, MenuItem,} from '@mui/material';
-
+import FlexBetween from 'components/FlexBetween';
+import profileImage from 'assets/profile.jpg';
 
 const Navbar = ({ user, isSidebarOpen, setIsSidebarOpen }) => {
+  const dispatch = useDispatch();
+  const theme = useTheme();
 
-    const dispatch = useDispatch();
-    const theme = useTheme();
+  const [anchorEl, setAnchorEl] = useState(null);
+  const isOpen = Boolean(anchorEl);
+  const handleClick = (event) => setAnchorEl(event.currentTarget);
+  const handleClose = () => setAnchorEl(null);
 
-        const [anchorEl, setAnchorEl] = useState(null);
-        const isOpen = Boolean(anchorEl);
-        const handleClick = (event) => setAnchorEl(event.currentTarget);
-        const handleClose = () => setAnchorEl(null);
+  return (
+    <AppBar
+      sx={{
+        position: "static",
+        background: "none",
+        boxShadow: "none",
+        zIndex: (theme) => theme.zIndex.drawer + 1,
+      }}
+    >
+      <Toolbar>
+        <FlexBetween width="100%">
+          {/* LEFT SIDE */}
+          <FlexBetween>
+            <IconButton onClick={() => setIsSidebarOpen((prev) => !prev)}>
+              <MenuIcon />
+            </IconButton>
+            <FlexBetween
+              backgroundColor={theme.palette.background.alt}
+              borderRadius="9px"
+              gap="3rem"
+              p="0.1rem 1.5rem"
+            >
+              <InputBase placeholder="Search..." />
+              <IconButton>
+                <Search />
+              </IconButton>
+            </FlexBetween>
+          </FlexBetween>
 
-  return <AppBar sx={{ position: "static", background: "none", boxShadow: "none", zIndex: (theme) => theme.zIndex.drawer + 1,}}>
-<Toolbar>
-  <FlexBetween width="100%">
-    
-    {/* LEFT SIDE */}
-    <FlexBetween>
-      <IconButton onClick={() => setIsSidebarOpen((prev) => !prev)}>
-        <MenuIcon />
-      </IconButton>
-      <FlexBetween
-        backgroundColor={theme.palette.background.alt}
-        borderRadius="9px"
-        gap="3rem"
-        p="0.1rem 1.5rem"
-      >
-        <InputBase placeholder="Search..." />
-        <IconButton>
-          <Search />
-        </IconButton>
-      </FlexBetween>
-    </FlexBetween>
+          {/* RIGHT SIDE */}
+          <FlexBetween gap="1.5rem">
+            {/* Theme toggle */}
+            <IconButton onClick={() => dispatch(setMode())}>
+              {theme.palette.mode === "dark" ? (
+                <DarkModeOutlined sx={{ fontSize: "25px" }} />
+              ) : (
+                <LightModeOutlined sx={{ fontSize: "25px" }} />
+              )}
+            </IconButton>
 
-    {/* RIGHT SIDE */}
-    <FlexBetween gap="1.5rem">
-      <IconButton onClick={() => dispatch(setMode())}>
-        {theme.palette.mode === "dark" ? (
-          <DarkModeOutlined sx={{ fontSize: "25px" }} />
-        ) : (
-          <LightModeOutlined sx={{ fontSize: "25px" }} />
-        )}
-      </IconButton>
-      <IconButton>
-        <SettingsOutlined sx={{ fontSize: "25px" }} />
-      </IconButton>
-    </FlexBetween>
-    
+            <IconButton>
+              <SettingsOutlined sx={{ fontSize: "25px" }} />
+            </IconButton>
 
-  </FlexBetween>
-</Toolbar>
-</AppBar>
+            {/* User Dropdown */}
+            <FlexBetween onClick={handleClick} sx={{ cursor: "pointer" }}>
+              <Avatar alt="profile" src={profileImage} sx={{ width: 32, height: 32, mr: 1 }} />
+              <Typography variant="subtitle2" color="text.primary">
+                {user?.name || "Admin"}
+              </Typography>
+              <ArrowDropDownOutlined />
+            </FlexBetween>
+
+            <Menu
+              anchorEl={anchorEl}
+              open={isOpen}
+              onClose={handleClose}
+              anchorOrigin={{ vertical: "bottom", horizontal: "right" }}
+              transformOrigin={{ vertical: "top", horizontal: "right" }}
+            >
+              <MenuItem onClick={handleClose}>Profile</MenuItem>
+              <MenuItem onClick={handleClose}>Logout</MenuItem>
+            </Menu>
+          </FlexBetween>
+        </FlexBetween>
+      </Toolbar>
+    </AppBar>
+  );
 };
 
-export default Navbar
+export default Navbar;
