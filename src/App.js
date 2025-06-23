bdb1f3752cf163b57ef15773ce6e1f51f7babcb6
Pree-15 <146547284+Pree-15@users.noreply.github.com>
2025-06-23 15:26:46 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,9 +1,9 @@
 import { CssBaseline, ThemeProvider } from "@mui/material";
 import { createTheme } from "@mui/material/styles";
-import { useSelector } from "react-redux";
-import { themeSettings } from "theme";
-import { useMemo } from "react";
 import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
+import { themeSettings } from "theme";
+
+// Pages
 import Dashboard from "scenes/dashboard";
 import Layout from "scenes/layout";
 import UploadProducts from "scenes/uploadProducts";
@@ -14,32 +14,30 @@ import Expense from "scenes/expense";
 import Reviews from "scenes/reviews";
 import Advertisement from "scenes/advertisement";
 
-
-
 function App() {
-  const mode = useSelector((state) => state.global.mode);
-  const theme = useMemo(() => createTheme(themeSettings(mode)), [mode]);
+  // Create theme once, since it's always light mode
+  const theme = createTheme(themeSettings);
+
   return (
     <div className="app">
       <BrowserRouter>
         <ThemeProvider theme={theme}>
           <CssBaseline />
-            <Routes>
-                <Route element={<Layout />}>
-                  <Route path="/" element={<Navigate to="/dashboard" replace />} />
-                  <Route path="/dashboard" element={<Dashboard />} />
-                  <Route path="/upload-products" element={<UploadProducts />} />
-                  <Route path="/orders" element={<Orders/>} />
-                  <Route path="/delivery-tracking" element={<DeliveryTracking />} />
-                  <Route path="/stocks" element={<Stocks />} />
-                  <Route path="/expense" element={<Expense />} />
-                  <Route path="/reviews-&-ratings" element={<Reviews />} />
-                  <Route path="/advertisement" element={<Advertisement />} />
-                </Route>
-              </Routes>
+          <Routes>
+            <Route element={<Layout />}>
+              <Route path="/" element={<Navigate to="/dashboard" replace />} />
+              <Route path="/dashboard" element={<Dashboard />} />
+              <Route path="/upload-products" element={<UploadProducts />} />
+              <Route path="/orders" element={<Orders />} />
+              <Route path="/delivery-tracking" element={<DeliveryTracking />} />
+              <Route path="/stocks" element={<Stocks />} />
+              <Route path="/expense" element={<Expense />} />
+              <Route path="/reviews-&-ratings" element={<Reviews />} />
+              <Route path="/advertisement" element={<Advertisement />} />
+            </Route>
+          </Routes>
         </ThemeProvider>
       </BrowserRouter>
-      
     </div>
   );
 }
